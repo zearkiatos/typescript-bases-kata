62637ddd6b2bfd914b9147f50cfd46c012b9317a
Pedro Capriles M <p0c04cw@M-C02Z9029LVCG.local>
2022-03-30 14:43:04 -0300

    [Work in Progress] Never

---
+++ tests/types/never.unit.test.ts
@@ -0,0 +1,12 @@
+import { fakeError } from '../../src/types/never';
+describe('Unit test suite for numbers type', () => {
+    test('Should generate an error', () => {
+        const errorExpected: string = '❌ Error ocurred 😱';
+        try {
+            fakeError('❌ Error ocurred 😱');
+        }
+        catch(ex:any) {              
+            expect(ex).toThrow();
+        }
+    });
+});
