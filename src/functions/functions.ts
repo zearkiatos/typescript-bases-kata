fb72a37f56ea550cc3d8617ac6a0521ba0b847b9
zearkiatos <caprilespe@outlook.com>
2022-04-05 20:14:18 -0400

    Basics functions

---
+++ src/functions/functions.ts
@@ -0,0 +1,11 @@
+const hero: string = "Flash";
+
+function returnName(): string {
+  return hero;
+}
+
+const activateBatiSignal = (): string => {
+  return "Batisignal activated!";
+};
+
+export { returnName, activateBatiSignal };
---
+++ tests/functions/functions.unit.test.ts
@@ -0,0 +1,19 @@
+import { returnName, activateBatiSignal } from '../../src/functions/functions';
+describe('Unit test suite for functions', () => {
+    test('Should execute a function and return the name of some hero', () => {
+        const expectedResult: string = 'Flash';
+
+        const hero: string = returnName();
+
+        expect(hero).toBe(expectedResult);
+    });
+
+    test('Should execute a function and return a message for Batman', () => {
+        const expectedResult: string = 'Batisignal activated!';
+
+        const messageForBatman: string = activateBatiSignal();
+
+        expect(typeof activateBatiSignal).toBe('function')
+        expect(messageForBatman).toBe(expectedResult);
+    });
+})
